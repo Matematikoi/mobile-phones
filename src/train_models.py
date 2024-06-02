b0718c113cbc54b2783c3d8fed03be0a04026d4b
Gabriel Lozano <37823629+Matematikoi@users.noreply.github.com>
2024-06-03 00:53:24 +0200

    Adds Model (#2)
    * Adds Model
    
    Adds CellPhoneModel class that makes it possible to predict and measure
    the effectiveness of a given model
    
    * small fix to predict
    
    * Adds pickle
    
    * pickle
    
    * Adds regression models
    
    * Delete src/Mobile Price Prediction (ML).ipynb
    
    * Added the EDA
    
    * Delete src/Mobile Price Prediction (ML)-EDA.ipynb
    
    * Added EDA
    
    * Adds a final readme adjustment
    
    * Small update in readme
    
    * Add instruction txt file
    
    * Adds output to the main file
    
    * Update Instruction.txt
    
    * Adds confusion matrix and results in a csv file
    
    * uncomment model lines
    
    * Deployment Added
    
    * Delete src/Mobile Price directory
    
    * Deployment of the mobile price app
    
    ---------
    
    Co-authored-by: Gabriel Lozano <golozanop@unal.edu.co>
    Co-authored-by: Pce-iyos <61274755+Pce-iyos@users.noreply.github.com>

---
+++ src/train_models.py
@@ -0,0 +1,272 @@
+# Ignore warnings for convergence
+# since scikit learn has problems ignoring them when n_jobs = -1
+# this solution is provided to ignore almost all warnings.
+#
+#
+# The code should run in ~1m30s
+import sys
+import os
+import warnings
+if not sys.warnoptions:
+    warnings.simplefilter("ignore")
+    os.environ["PYTHONWARNINGS"] = "ignore"
+import file_management as fm
+import matplotlib.pyplot as plt
+from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix
+from sklearn.exceptions import ConvergenceWarning
+from sklearn.utils._testing import ignore_warnings
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import make_pipeline
+from sklearn.linear_model import LogisticRegression, Ridge, LinearRegression, Lasso
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import f1_score, make_scorer, d2_absolute_error_score
+import pandas as pd
+import numpy as np
+import pickle
+
+
+class CellPhoneModel:
+    available_models = [
+        "LOGISTIC_REGRESSION",
+        "SVC",
+        "DECISION_TREE",
+        "RANDOM_FOREST",
+        "GRADIENT_BOOSTING",
+        "RIDGE_REGRESSION",
+        "LINEAR_REGRESSION",
+        "LASSO",
+    ]
+
+    def __init__(self, model_name, params):
+        """
+        An instance of the CellPhoneModel will perform a CV hyperparameter
+        tunning using the training dataset.
+
+        We pass the model we want to use as model, and the parameters we want to use as
+        params
+        """
+        if model_name not in self.available_models:
+            raise Exception(
+                f'Model "{model_name}" not implemented, available models are {self.available_models}'
+            )
+        self.model_name = model_name
+        self.params = params
+
+    def __is_regression(self):
+        regression_models = self.available_models[5:]
+        if self.model_name in regression_models:
+            return True
+        else:
+            return False
+
+    def get_data(self):
+        """
+        Get the data using file_management.pu
+        """
+        # Get Data
+        target = "price_range"
+        data = fm.read_parquet(fm.Filenames.train)
+        X = data.drop(columns=target)
+        y = data[target]
+
+        X_val_train, self.X_test, y_val_train, self.y_test = train_test_split(
+            X, y, test_size=0.2, random_state=42
+        )
+        self.X_train, self.X_val, self.y_train, self.y_val = train_test_split(
+            X_val_train, y_val_train, test_size=0.3, random_state=42
+        )
+
+    @ignore_warnings(category=ConvergenceWarning)
+    def train(self):
+        """
+        Trains the model and saves the model data
+        """
+        self.get_data()
+
+        pipeline_params = [StandardScaler()]
+        # Switch for the model
+        if self.model_name == "LOGISTIC_REGRESSION":
+            pipeline_params.append(LogisticRegression(random_state=42))
+        elif self.model_name == "SVC":
+            pipeline_params.append(SVC(random_state=42))
+        elif self.model_name == "DECISION_TREE":
+            pipeline_params.append(DecisionTreeClassifier(random_state=42))
+        elif self.model_name == "RANDOM_FOREST":
+            pipeline_params.append(RandomForestClassifier(random_state=42))
+        elif self.model_name == "GRADIENT_BOOSTING":
+            pipeline_params.append(GradientBoostingClassifier(random_state=42))
+        elif self.model_name == "RIDGE_REGRESSION":
+            pipeline_params.append(Ridge(random_state=42))
+        elif self.model_name == "LINEAR_REGRESSION":
+            pipeline_params.append(LinearRegression())
+        elif self.model_name == "LASSO":
+            pipeline_params.append(Lasso(random_state=42))
+
+        model = make_pipeline(*pipeline_params)
+        f1 = make_scorer(f1_score, average="micro")
+
+        if not self.__is_regression():
+            model_grid = GridSearchCV(
+                model, param_grid=self.params, cv=5, n_jobs=-1, verbose=1, scoring=f1
+            )
+        else:
+            model_grid = GridSearchCV(
+                model,
+                param_grid=self.params,
+                cv=5,
+                n_jobs=-1,
+                verbose=1,
+                scoring=make_scorer(d2_absolute_error_score),
+            )
+        model_grid.fit(self.X_train, self.y_train)
+        self.model = model_grid
+
+    def get_score_val(self):
+        """
+        Get the f1 micro result for validation
+        """
+        y_pred = self.predict(self.X_val)
+        if self.__is_regression():
+            y_pred = np.minimum(y_pred.round().astype(int), 3)
+        return f1_score(self.y_val, y_pred, average="micro")
+
+    @ignore_warnings(category=ConvergenceWarning)
+    def get_score_test(self):
+        """
+        Get the f1 micro result for test
+        """
+        X = np.concatenate((self.X_train, self.X_val), axis=0)
+        y = np.concatenate((self.y_train, self.y_val), axis=0)
+        self.model.fit(X, y)
+        y_pred = self.predict(self.X_test)
+        if self.__is_regression():
+            y_pred = np.minimum(y_pred.round().astype(int), 3)
+        return f1_score(self.y_test, y_pred, average="micro")
+
+    @ignore_warnings(category=ConvergenceWarning)
+    def train_final_model(self):
+        """
+        Trains the model with all the data
+        """
+        X = np.concatenate((self.X_train, self.X_val, self.X_test), axis=0)
+        y = np.concatenate((self.y_train, self.y_val, self.y_test), axis=0)
+        self.model.fit(X, y)
+
+    def predict(self, data):
+        """
+        Given a matrix X it predict a vector y
+        """
+        return self.model.predict(data)
+
+def save_confusion_matrix(model):
+    cm = confusion_matrix(model.y_test, model.predict(model.X_test))
+    d  = ConfusionMatrixDisplay(confusion_matrix = cm)
+    d.plot();
+    plt.savefig('./data/confusion_matrix.png')
+
+def main():
+    model_params = [
+        (
+            "LOGISTIC_REGRESSION",
+            {
+                "logisticregression__C": [0.001, 0.1, 0.5, 1.0],
+                "logisticregression__solver": [
+                    "lbfgs",
+                    "newton-cg",
+                    "newton-cholesky",
+                    "sag",
+                    "saga",
+                ],
+                "logisticregression__penalty": ["l1", "l2", "elasticnet"],
+                "logisticregression__max_iter": [100, 10000],
+            },
+        ),
+        (
+            "SVC",
+            {
+                "svc__C": [0.001, 0.1, 0.5, 1.0],
+                "svc__gamma": [1, 0.1, 0.01, 0.001, 0.0001, "scale", "auto"],
+                "svc__kernel": ["rbf", "linear", "poly", "sigmoid", "precomputed"],
+            },
+        ),
+        (
+            "DECISION_TREE",
+            {
+                "decisiontreeclassifier__criterion": ["gini", "entropy"],
+                "decisiontreeclassifier__splitter": ["best", "random"],
+                "decisiontreeclassifier__max_depth": [3, 5, 7, 10],
+                "decisiontreeclassifier__min_samples_split": range(2, 11, 1),
+                "decisiontreeclassifier__min_samples_leaf": range(2, 10, 1),
+            },
+        ),
+        (
+            "RANDOM_FOREST",
+            {
+                "randomforestclassifier__n_estimators": range(25, 151, 50),
+                "randomforestclassifier__criterion": ["gini", "entropy"],
+                "randomforestclassifier__max_depth": [5, 7, 10],
+                "randomforestclassifier__min_samples_split": range(2, 100, 30),
+                "randomforestclassifier__min_samples_leaf": range(2, 100, 30),
+            },
+        ),
+        (
+            "GRADIENT_BOOSTING",
+            {
+                "gradientboostingclassifier__n_estimators": range(25, 151, 25),
+                "gradientboostingclassifier__max_depth": [3, 5, 7, 10],
+            },
+        ),
+        (
+            "RIDGE_REGRESSION",
+            {
+                "ridge__alpha": np.logspace(-6, 6, 13),
+                "ridge__max_iter": [1, 3, 30, 100, 200],
+            },
+        ),
+        ("LINEAR_REGRESSION", {"linearregression__fit_intercept": [True, False]}),
+        (
+            "LASSO",
+            {
+                "lasso__fit_intercept": [True, False],
+                "lasso__alpha": np.logspace(-6, 6, 13),
+            },
+        ),
+    ]
+
+    result = []
+    for model, param in model_params:
+        print(f"Training model {model}")
+        m = CellPhoneModel(model, param)
+        m.train()
+        result.append(m)
+    models = [(i.get_score_test(), i) for i in result]
+    for score, model in models:
+        print(f'f1 score for {model.model_name} is {score}')
+
+    df_results = pd.DataFrame({
+        'model_name':[i[1].model_name for i in models],
+        'f1_score':[i[0] for i in models]
+    })
+
+    df_results.to_csv('./data/results.csv')
+    _, best_model = max(models)
+    print('Training the final model with all the information')
+    best_model.train_final_model()
+    with open("./data/model.pickle", "wb") as handle:
+        pickle.dump(best_model, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    # Generate confusion matrix
+    save_confusion_matrix(best_model)
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
